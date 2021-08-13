f1985edd27d77cd6e667f8b72e9ca92c54cdc891
Akash A S <84561736+akash-ashok@users.noreply.github.com>
2021-08-13 22:12:04 +0530

    product details updated

--- src/Routes/ProductUploadForm/index.js
+++ src/Routes/ProductUploadForm/index.js
@@ -4,24 +4,26 @@ const ProductForm = () => {
     return (
      <div>
          <h1>Product Details</h1>
-        <div className="productname">Name of Product <br />
-            <input type="text" />
-        </div>
-        <div className="product_category"> Product Category : 
+
+         <div className="basic_details">
+           <div className="productname">Name of Product <br />
+              <input className="name_product_box" type="text" />
+            </div>
+           <div className="product_category"> Product Category : 
             <select name="category" id="category">
                 <option value="NULL">NULL</option>
             </select>
             <div className="price">
                 Price 
-                <input type="number" />
+                <input className='price_box' type="number" />
             </div>
             <div className="numberofstock">
                 Number of Stocks 
                 <input type="number" />
             </div>
             <div className="about">
-                About Product 
-                <input type="text" />
+                About Product <br/>
+                <input className="about_box" type="text" />
             </div>
             <div className="uploadimage">
                 Upload Image
@@ -34,7 +36,8 @@ const ProductForm = () => {
 
         </div>
 
-     </div>
+         </div>
+         </div>
     )
 }
 
